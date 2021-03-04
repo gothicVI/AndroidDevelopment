45268762ab9602c00dac7fa93111ba5445ff9f8b
Sebastian Steinbeißer <sebastian.steinbeisser@googlemail.com>
2021-03-04 18:19:02 +0100

    Add remaining 2021-03 security patches for...
    ...los 15.1, 16.0, 17.1, and 18.1

--- laos_build.sh
+++ laos_build.sh
@@ -87,30 +87,30 @@ function pick_unmerged_commits {
     fi
     if [ "${rev}" == "15.1" ]; then
         echo
-        #2021-02-05
+        #2021-03-05
         source "${HOME}/git/AndroidDevelopment/lineageos-gerrit-repopick-topic.sh"
-        repopick_topic O_asb_2021-02 || exit 1
+        repopick_topic O_asb_2021-03 || exit 1
     fi
     if [ "${rev}" == "16.0" ]; then
         echo
-        #2021-02-05
+        #2021-03-05
         source "${HOME}/git/AndroidDevelopment/lineageos-gerrit-repopick-topic.sh"
-        repopick_topic P_asb_2021-02 || exit 1
+        repopick_topic P_asb_2021-03 || exit 1
     fi
     if [ "${rev}" == "17.1" ]; then
         echo
-        #2021-02-05
+        #2021-03-05
         source "${HOME}/git/AndroidDevelopment/lineageos-gerrit-repopick-topic.sh"
-        repopick_topic Q_asb_2021-02 || exit 1
+        repopick_topic Q_asb_2021-03 || exit 1
         echo
         #soong: java: Specify larger heap size for metalava
 #        repopick -f 266411 2>&1 || exit 1
     fi
     if [ "${rev}" == "18.1" ]; then
         echo
-        #2021-02-05
+        #2021-03-05
         source "${HOME}/git/AndroidDevelopment/lineageos-gerrit-repopick-topic.sh"
-        repopick_topic android-11.0.0_r29 || exit 1
+        repopick_topic android-11.0.0_r32 || exit 1
         echo
         #soong: java: Specify larger heap size for metalava
 #        repopick -f 289926 2>&1 || exit 1
