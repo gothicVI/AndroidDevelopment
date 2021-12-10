46e0b83028952f12c8889888aa9a2f673dd45c81
Sebastian Steinbeißer <sebastian.steinbeisser@googlemail.com>
2021-12-10 07:21:35 +0100

    Avoid issues with base64 encoding in repopick_topic
    This currently relies on picking the manifest changes, copying the
    manifest and syncing again - basically what repopick_topic does as well
    but less elegant.
    Maybe this can be improved...

--- laos_build.sh
+++ laos_build.sh
@@ -87,33 +87,32 @@ function pick_unmerged_commits {
         #2021-10-05
         #2021-11-05
         #2021-12-05
-        source "${HOME}/git/AndroidDevelopment/lineageos-gerrit-repopick-topic.sh"
-        repopick_topic n-asb-2021-07 || exit 1
+        repopick -t n-asb-2021-07 || exit 1
         echo
         local_security_patch_level || exit 1
         echo
         read -s
-        repopick_topic n-asb-2021-08 || exit 1
+        repopick -t n-asb-2021-08 || exit 1
         echo
         local_security_patch_level || exit 1
         echo
         read -s
-        repopick_topic n-asb-2021-09 || exit 1
+        repopick -t n-asb-2021-09 || exit 1
         echo
         local_security_patch_level || exit 1
         echo
         read -s
-        repopick_topic n-asb-2021-10 || exit 1
+        repopick -t n-asb-2021-10 || exit 1
         echo
         local_security_patch_level || exit 1
         echo
         read -s
-        repopick_topic n-asb-2021-11 || exit 1
+        repopick -t n-asb-2021-11 || exit 1
         echo
         local_security_patch_level || exit 1
         echo
         read -s
-        repopick_topic n-asb-2021-12 || exit 1
+        repopick -t n-asb-2021-12 || exit 1
         echo
         local_security_patch_level || exit 1
         echo
@@ -122,20 +121,17 @@ function pick_unmerged_commits {
     if [ "${rev}" == "15.1" ]; then
         echo
         #2021-10-05
-        source "${HOME}/git/AndroidDevelopment/lineageos-gerrit-repopick-topic.sh"
-        repopick_topic O_asb_2021-10 || exit 1
+        repopick -t O_asb_2021-10 || exit 1
     fi
     if [ "${rev}" == "16.0" ]; then
         echo
         #2021-11-05
-        source "${HOME}/git/AndroidDevelopment/lineageos-gerrit-repopick-topic.sh"
-        repopick_topic P_asb_2021-11 || exit 1
+        repopick -t P_asb_2021-11 || exit 1
     fi
     if [ "${rev}" == "17.1" ]; then
         echo
         #2021-11-05
-        source "${HOME}/git/AndroidDevelopment/lineageos-gerrit-repopick-topic.sh"
-        repopick_topic Q_asb_2021-11 || exit 1
+        repopick -t Q_asb_2021-11 || exit 1
         echo
         #soong: java: Specify larger heap size for metalava
 #        repopick -f 266411 2>&1 || exit 1
@@ -143,8 +139,10 @@ function pick_unmerged_commits {
     if [ "${rev}" == "18.1" ]; then
         echo
         #2021-12-05
-        source "${HOME}/git/AndroidDevelopment/lineageos-gerrit-repopick-topic.sh"
-        repopick_topic R_asb_2021-12 || exit 1
+        repopick 319964 || exit 1
+        cp android/default.xml .repo/manifests || exit 1
+        repo sync -v -j 1 -c --no-tags --no-clone-bundle --force-sync --fail-fast 2>&1 || exit 1
+        repopick -t R_asb_2021-12 || exit 1
         echo
         #soong: java: Specify larger heap size for metalava
 #        repopick -f 289926 2>&1 || exit 1
@@ -152,8 +150,10 @@ function pick_unmerged_commits {
     if [ "${rev}" == "19.0" ]; then
         echo
         #2021-12-05
-        source "${HOME}/git/AndroidDevelopment/lineageos-gerrit-repopick-topic.sh"
-        repopick_topic android-12.0.0_r16 || exit 1
+        repopick 319963 || exit 1
+        cp android/default.xml .repo/manifests || exit 1
+        repo sync -v -j 1 -c --no-tags --no-clone-bundle --force-sync --fail-fast 2>&1 || exit 1
+        repopick -t android-12.0.0_r16 || exit 1
         echo
     fi
     return 0
