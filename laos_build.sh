bd614289aa7ef70aba39ad2fc4a115541f29db3d
Sebastian Steinbeißer <sebastian.steinbeisser@googlemail.com>
2021-12-15 07:24:37 +0100

    Continue 708b726cf5caa2658c575d13d690016e9a363636

--- laos_build.sh
+++ laos_build.sh
@@ -130,8 +130,11 @@ function pick_unmerged_commits {
     fi
     if [ "${rev}" == "17.1" ]; then
         echo
-        #2021-11-05
-        repopick -t Q_asb_2021-11 || exit 1
+        #2021-12-05
+        repopick 320284 || exit 1
+        cp android/default.xml .repo/manifests || exit 1
+        repo sync -v -j 1 -c --no-tags --no-clone-bundle --force-sync --fail-fast 2>&1 || exit 1
+        repopick -t Q_asb_2021-12 || exit 1
         echo
         #soong: java: Specify larger heap size for metalava
 #        repopick -f 266411 2>&1 || exit 1
