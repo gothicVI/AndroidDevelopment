6842be1b36b797d754eb7a808e0dfa1d5dab14e1
Sebastian Steinbeißer <sebastian.steinbeisser@googlemail.com>
2022-08-04 18:32:59 +0200

    August security patches

--- laos_build.sh
+++ laos_build.sh
@@ -128,15 +128,18 @@ function pick_unmerged_commits {
     fi
     if [ "${rev}" == "17.1" ]; then
         echo
-        #2022-07-05
-        repopick -t Q_asb_2022-07 || exit 1
+        #2022-08-05
+        repopick -f 334780 || exit 1
+        cp -v ./android/default.xml ./.repo/manifests || exit 1
+        repo sync -v -j 1 -c --no-tags --no-clone-bundle --force-sync --fail-fast packages/services/Car 2>&1 || exit 1
+        repopick -t Q_asb_2022-08 || exit 1
         echo
     fi
     if [ "${rev}" == "18.1" ]; then
         echo
-        #2022-07-05
+        #2022-08-05
         #repopick -Q "topic:R_asb_2022-06+NOT+332370" || exit 1
-        repopick -t R_asb_2022-07 || exit 1
+        repopick -t R_asb_2022-08 || exit 1
         echo
     fi
     if [ "${rev}" == "19.1" ]; then
