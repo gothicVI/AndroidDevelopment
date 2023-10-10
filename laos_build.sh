c2b3b0426a8bd9671f830016411cb6c4c0084c9a
Sebastian Steinbeißer <sebastian.steinbeisser@googlemail.com>
2023-10-10 18:58:45 +0200

    October security patches

--- laos_build.sh
+++ laos_build.sh
@@ -137,6 +137,8 @@ function pick_unmerged_commits {
         repopick -f -t n-asb-2023-08 || exit 1
         #2023-09-05
         repopick -f -t n-asb-2023-09 || exit 1
+        #2023-10-05
+        repopick -f -t n-asb-2023-10 || exit 1
         #tzdb2021c_N
         repopick -f -t tzdb_N || exit 1
         echo
@@ -220,34 +222,36 @@ function pick_unmerged_commits {
         repo sync -v -j 1 -c --no-tags --no-clone-bundle --force-sync --fail-fast 2>&1 system/ca-certificates || exit 1
         repopick -f -t Q_asb_2023-08 || exit 1
         echo
+        #2023-09-05
+        repopick -f -t Q_asb_2023-09 || exit 1
     fi
     if [ "${rev}" == "18.1" ]; then
         echo
-        #2023-09-05
-        # repopick -f 364026 || exit 1
-        # cp -v ./android/default.xml ./.repo/manifests/ || exit 1
-        # repo sync -v -j 1 -c --no-tags --no-clone-bundle --force-sync --fail-fast 2>&1 packages/apps/QuickAccessWallet system/ca-certificates || exit 1
-        repopick -t R_asb_2023-09 || exit 1
+        #2023-10-05
+        repopick -f 368054 || exit 1
+        cp -v ./android/default.xml ./.repo/manifests/ || exit 1
+        repo sync -v -j 1 -c --no-tags --no-clone-bundle --force-sync --fail-fast 2>&1 external/libxml2 || exit 1
+        repopick -t R_asb_2023-10 || exit 1
         echo
     fi
     if [ "${rev}" == "19.1" ]; then
         echo
-        #2023-09-05
-        # repopick -f 363979 || exit 1
-        # cp -v ./android/default.xml ./.repo/manifests/ || exit 1
-        # repo sync -v -j 1 -c --no-tags --no-clone-bundle --force-sync --fail-fast 2>&1 external/aac packages/apps/ManagedProvisioning system/ca-certificates || exit 1
-        repopick -t S_asb_2023-09 || exit 1
+        #2023-10-05
+        repopick -f 368027 || exit 1
+        cp -v ./android/default.xml ./.repo/manifests/ || exit 1
+        repo sync -v -j 1 -c --no-tags --no-clone-bundle --force-sync --fail-fast 2>&1 external/libxml2 || exit 1
+        repopick -t S_asb_2023-10 || exit 1
         echo
     fi
     if [ "${rev}" == "20.0" ]; then
         echo
-        #2023-09-05
-        repopick -f 366308 || exit 1
+        #2023-10-05
+        repopick -f 367945 || exit 1
         cp -v ./android/default.xml ./.repo/manifests/ || exit 1
-        cp -v ./android/snippets/lineage.xml ./.repo/manifests/snippets/ || exit 1
-        cp -v ./android/snippets/pixel.xml ./.repo/manifests/snippets/ || exit 1
-        repo sync -v -j 1 -c --no-tags --no-clone-bundle --force-sync --fail-fast 2>&1 || exit 1
-        repopick -p -t T_asb_2023-09 || exit 1
+        # cp -v ./android/snippets/lineage.xml ./.repo/manifests/snippets/ || exit 1
+        # cp -v ./android/snippets/pixel.xml ./.repo/manifests/snippets/ || exit 1
+        repo sync -v -j 1 -c --no-tags --no-clone-bundle --force-sync --fail-fast 2>&1 external/libxml2 tools/apksig || exit 1
+        repopick -p -t T_asb_2023-10 || exit 1
         echo
     fi
     return 0
